2b496c2fe10a8cd99adec432cd907a8bcc5aadc5
Cosmin Baies <cb3618@ic.ac.uk>
2022-01-27 12:50:18 +0200

    Fix test

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -12,7 +12,7 @@ public class QueryProcessorTest {
 
     @Test
     public void returnsEmptyStringIfCannotProcessQuery() throws Exception {
-        assertThat(queryProcessor.process("test"), is(""));
+        assertThat(queryProcessor.process("test"), is("Sorry, we cannot handle that query yet!"));
     }
 
     @Test
